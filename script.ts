bdfc6375109d37f00e254e0ffdb70fbce987ee1e
Michael Woolnough <michael.woolnough@gmail.com>
2022-12-27 18:45:59 +0000

    set language appropriate document title

--- script.ts
+++ script.ts
@@ -1,3 +1,12 @@
+import {clearNode} from './lib/dom.js';
 import pageLoad from './lib/load.js';
+import lang from './language.js';
 
-pageLoad.then(() => {});
+pageLoad.then(() => {
+	const title = document.head.getElementsByTagName("title")[0];
+	if (title) {
+		clearNode(title, lang["TITLE"]);
+	} else {
+		document.title = lang["TITLE"] + "";
+	}
+});
