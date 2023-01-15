7f834f0e162ae1388a22e5c39247ae4f840da554
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-15 17:16:36 +0000

    added shell to window + create layers dock

--- script.ts
+++ script.ts
@@ -1,12 +1,34 @@
-import {clearNode} from './lib/dom.js';
+import {add, render} from './lib/css.js';
+import {amendNode, clearNode} from './lib/dom.js';
+import {circle, g, rect, svg, title} from './lib/svg.js';
 import pageLoad from './lib/load.js';
 import lang from './language.js';
+import layers from './layers.js';
+import {desktop, dockWindow, shell} from './docks.js';
 
 pageLoad.then(() => {
-	const title = document.head.getElementsByTagName("title")[0];
-	if (title) {
-		clearNode(title, lang["TITLE"]);
+	add({
+		"dock-shell": {
+			"position": "absolute",
+			"top": 0,
+			"left": 0,
+			"right": 0,
+			"bottom": 0,
+			"overflow": "clip"
+		}
+	});
+	document.head.append(render());
+	const t = document.head.getElementsByTagName("title")[0],
+	      s = svg();
+	if (t) {
+		clearNode(t, lang["TITLE"]);
 	} else {
 		document.title = lang["TITLE"] + "";
 	}
+	clearNode(document.body, shell);
+	amendNode(desktop, s);
+	amendNode(shell, dockWindow({"window-title": "Layers", "docked": true}, [
+		layers({"svg": s}),
+		s
+	]));
 });
