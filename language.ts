4e289b5fbe48738e1acb8a6a5e5b02fa746b534b
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-16 17:21:32 +0000

    added language entries

--- language.ts
+++ language.ts
@@ -19,6 +19,10 @@ makeLangPack = <T extends Record<string, string>>(base: T, alternates: Record<st
 languages = ["en-GB", "en-US"];
 
 export default makeLangPack({
+	"CONTROL_DOCK_DOWN": "Move Down",
+	"CONTROL_DOCK_LEFT": "Dock to Left",
+	"CONTROL_DOCK_RIGHT": "Dock to Right",
+	"CONTROL_DOCK_UP": "Move Up",
 	"DOCK_LAYERS": "Layers",
 	"TITLE": "Skiving - SVG Generator",
 	"TITLE_CIRCLE": "Circle",
