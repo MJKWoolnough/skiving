2f2e8bb9b062619634d47c2572b9549074af7517
Michael Woolnough <michael.woolnough@gmail.com>
2023-01-27 17:37:15 +0000

    need to reset last var

--- docks.ts
+++ docks.ts
@@ -103,6 +103,7 @@ class DockShell extends ShellElement {
 			amendNode(this.#left[i][0], {"style": {"--window-left": 0, "--window-top": last + "%", "--window-width": leftWidth, "--window-height": s + "%"}});
 			last += s;
 		}
+		last = 0;
 		for (let i = 0; i < this.#right.length; i++) {
 			const s = this.#rightSplits[i];
 			amendNode(this.#right[i][0], {"style": {"--window-left": `calc(100% - ${rightWidth}`, "--window-top": last + "%", "--window-width": rightWidth, "--window-height": s + "%"}});
