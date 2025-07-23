163c1a7a0b427f68e092cc23328a78cf13371e38
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-23 07:24:55 +0100

    re-formatted

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 (
 	head -n5 index.html;
-	echo -n "		<script type=\"module\">"
+	echo -n "		<script type=\"module\">";
 	jspacker -i /script.js -n | terser -m --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
 	echo "</script>";
 	tail -n5 index.html;
